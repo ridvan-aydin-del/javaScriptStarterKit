106d427c40c6b084693696c2ee65153c5cdfa289
Rıdvan-Aydın-Del <aydinridvan2834@gmail.com>
2023-03-01 15:59:22 +0300

    JavaScript 1. Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 console.log("Merhaba")
+
+var dolarDun = 9.20
+var dolarBugun = 9.30
+
+{
+    let dolarDun=9.10
+}
+
+const euroDun = 11.2
+
+console.log(dolarDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)
