0cb28883ca74fed11c12aa1c2f0c7748bb1d861b
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2023-03-02 15:13:47 +0300

    JavaScript 2. Ders Sonu

--- src/functions.js
+++ src/functions.js
@@ -41,3 +41,49 @@ let product3 = {productName:"Elma", unitPrice:10, quantity:5}
 product2 = product3
 product2.productName = "Karpuz"
 console.log(product3.productName)
+
+function addToCart4(products) {
+    console.log(products)
+}
+let products = [
+    {productName:"Elma", unitPrice:10, quantity:5},
+    {productName:"Armut", unitPrice:10, quantity:5},
+    {productName:"Karpuz", unitPrice:10, quantity:5}
+]
+addToCart4(products)
+
+function add(...numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i]
+        
+    }
+    console.log("Toplam: ",total)
+}
+add(5,5)
+add(50,60,70)
+add(50,60,70,100,623,12)
+
+
+let numbers = [30,50,70,100]
+console.log(Math.max(...numbers))
+
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
+    {name:"İç Anadolu",population:"20M"},
+    {name:"Marmara", population:"30M"},
+    {name:"Karadeniz", population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinop","Trabzon"],
+    ]
+]
+console.log(icAnadolu)
+console.log(marmara)
+console.log(icAnadoluSehirleri)
+
+let newProductName, newUnitPrice, newQuantity
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity}={productName:"Elma",unitPrice:10,quantity:5})
+console.log(newProductName)
+console.log(newUnitPrice)
+console.log(newQuantity)
