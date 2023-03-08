3016cc2f0ccd3b4d247a78d1b4ffbff27ccb81d2
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2023-03-08 12:13:09 +0300

    Ders 3 ders sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,12 +1,19 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
 console.log("User Component Yüklendi")
 
-let userService = new UserService()
+let logger1 = new BaseLogger()
+let userService = new UserService(logger1)
 
 let user1 = new User(1,"Engin","Demiroğ","Ankara");
-userService.add(user1.lastName)
+let user2 = new User(2,"Rıdvan","Aydın","Giresun");
+userService.add(user1)
+userService.add(user2)
+
+console.log(userService.list())
+console.log(userService.getById(2))
 
 userService.getById(1)
 userService.list()
